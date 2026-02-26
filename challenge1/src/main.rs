92f5dc1e198779b5e7667a37f31a4cd563b7ec13
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-02-26 10:32:36 -0300

    desafio 1 feito

---
+++ challenge1/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // use cargo run src/main.rs from the challenge1 folder
+    println!("My name is Davi Maciel Cavalcante");
+    println!("I want a girlfriend");
+    println!("I want to have more money");
+    println!("I want to be happy");
+}
