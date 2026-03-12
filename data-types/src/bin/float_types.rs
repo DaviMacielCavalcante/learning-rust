68dde438acbfe54edf08582980e9801a1b00e716
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-03-12 18:12:24 -0300

    casting, floats, format specifiers, methods

---
+++ data-types/src/bin/float_types.rs
@@ -0,0 +1,9 @@
+fn main() {
+
+    let pi: f64 = 3.141592653;
+    println!("The current value of pi in this code is {pi}");
+
+    println!("{}", pi.floor());
+    println!("{}", pi.ceil());
+    println!("{}", pi.round());
+}
---
+++ data-types/src/bin/format_specifier.rs
@@ -0,0 +1,7 @@
+fn main() {
+
+    let pi: f64 = 3.141592653;
+    println!("The current value of pi in this code is {pi:.4}");
+
+    println!("This works too {:.4}", pi);
+}
